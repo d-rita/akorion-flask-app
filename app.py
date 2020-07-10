9cbc5d69ed87b515daa229c9d99d5fca347f39f7
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-07-10 10:46:43 +0300

    refactor app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, make_response
+
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "ritananyanzi@gmail.com"
+app.config["MAIL_PASSWORD"] = "qwertyRandom"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    # render form 
+    return render_template('index.html')
+
+@app.route('/order', methods=["POST"])
+def submit_order():
+    # get form data
+    name = request.form["username"]
+    tel = request.form["phone"]
+    order_name = request.form["order"]
+
+    # validate data
+    if name == "" or tel == "" or order_name == "":
+        return "Fill in all values in the form"
+
+    # send valid data to email
+    email = Message(
+       subject="Customer Order",
+        body=f"Customer: {name} has placed an order for {order_name}. This is their contact: {tel}",
+        sender="ritananyanzi@gmail.com",
+        recipients=["martinkatamba@akorion.com"]
+    )
+    mail.send(email)
+    return render_template('final.html')
